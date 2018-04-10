df00a62869f87f839f759d5d1a7722260eac22e3
Daniel Martin <daniel@dirtyandnative.de>
2018-04-10 14:09:24 +0200

    a

---
+++ FATestApp/FATestApp/FATestApp/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FATestApp
+{
+	public partial class MainPage : ContentPage
+	{
+	    public string FaIcon { get; set; } = "\uf013";
+        
+        public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
